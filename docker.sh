5871bbee2e5fd5efec2226c0de15cbac2479216b
unknown <atazofack@hmail.com>
2023-02-18 22:00:27 -0500

    docker update

--- docker.sh
+++ docker.sh
@@ -19,24 +19,3 @@ sudo yum install docker-ce -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Post Installation Steps
-# Create Docker group
-sudo groupadd docker
-
-# Add user to the docker group
-sudo usermod -aG docker $USER
-
-echo "Installation Complete -- Logout and Log back"
-
-# Install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Permssion +x execute binary
-chmod +x /usr/local/bin/docker-compose
-
-# Create link symbolic 
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Check Version docer-compose
-echo "Installation Complete -- Logout and Log back"
-docker-compose --version
